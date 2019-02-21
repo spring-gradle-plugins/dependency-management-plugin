5a1b08337c7a7bdc1f18ce6ef3d56e34384833b3
Andy Wilkinson <awilkinson@pivotal.io>
2019-02-21 10:52:01 +0000

    Polish CI release staging configuration
    Closes gh-236

--- ci/scripts/stage.sh
+++ ci/scripts/stage.sh
@@ -13,19 +13,25 @@ git clone git-repo stage-git-repo > /dev/null
 pushd stage-git-repo > /dev/null
 
 snapshotVersion=$( awk -F '=' '$1 == "version" { print $2 }' gradle.properties )
-if [[ $RELEASE_TYPE = "RELEASE" ]]; then
-	stageVersion=$( strip_snapshot_suffix $snapshotVersion)
+if [[ $RELEASE_TYPE = "M" ]]; then
+	stageVersion=$( get_next_milestone_release $snapshotVersion)
+	nextVersion=$snapshotVersion
+elif [[ $RELEASE_TYPE = "RC" ]]; then
+	stageVersion=$( get_next_rc_release $snapshotVersion)
+	nextVersion=$snapshotVersion
+elif [[ $RELEASE_TYPE = "RELEASE" ]]; then
+	stageVersion=$( get_next_release $snapshotVersion)
 	nextVersion=$( bump_version_number $snapshotVersion)
 else
 	echo "Unknown release type $RELEASE_TYPE" >&2; exit 1;
 fi
 
 echo "Staging $stageVersion (next version will be $nextVersion)"
-sed -i "" "s/version=$snapshotVersion/version=$stageVersion/" gradle.properties
+sed -i "s/version=$snapshotVersion/version=$stageVersion/" gradle.properties
 
 git config user.name "Spring Buildmaster" > /dev/null
 git config user.email "buildmaster@springframework.org" > /dev/null
-git add pom.xml > /dev/null
+git add gradle.properties > /dev/null
 git commit -m"Release v$stageVersion" > /dev/null
 git tag -a "v$stageVersion" -m"Release v$stageVersion" > /dev/null
 
@@ -33,8 +39,8 @@ git tag -a "v$stageVersion" -m"Release v$stageVersion" > /dev/null
 
 git reset --hard HEAD^ > /dev/null
 echo "Setting next development version (v$nextVersion)"
-sed -i "" "s/version=$snapshotVersion/version=$nextVersion/" gradle.properties
-git add . > /dev/null
+sed -i "s/version=$snapshotVersion/version=$nextVersion/" gradle.properties
+git add gradle.properties > /dev/null
 git commit -m"Next development version (v$nextVersion)" > /dev/null
 
 echo "DONE"
