32fdf1684b6a1d5d3936fa3929992ba0eed38746
Andy Wilkinson <awilkinson@pivotal.io>
2019-02-22 13:01:12 +0000

    Correct GitHub org and repository used for release note generation
    See gh-236

--- ci/scripts/generate-release-notes.sh
+++ ci/scripts/generate-release-notes.sh
@@ -7,8 +7,8 @@ milestone=${version}
 java -jar /github-release-notes-generator.jar \
   --releasenotes.github.username=${GITHUB_USERNAME} \
   --releasenotes.github.password=${GITHUB_TOKEN} \
-  --releasenotes.github.organization=spring-projects \
-  --releasenotes.github.repository=spring-boot  \
+  --releasenotes.github.organization=spring-gradle-plugins \
+  --releasenotes.github.repository=dependency-management-plugin  \
   ${milestone} generated-release-notes/release-notes.md
 
 echo ${version} > generated-release-notes/version
