5e54e0d189e8055c8a7aa0f9f79f96181c36a164
Andy Wilkinson <wilkinsona@vmware.com>
2022-06-29 15:37:19 +0100

    Increase open files limit on CI to work around TestKit problem

--- ci/scripts/build-pr-project.sh
+++ ci/scripts/build-pr-project.sh
@@ -4,5 +4,6 @@ set -e
 source $(dirname $0)/common.sh
 
 pushd git-repo > /dev/null
+ulimit -n 65536
 ./gradlew --no-daemon -PdeploymentRepository=${repository} build uploadArchives
 popd > /dev/null
--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -5,5 +5,6 @@ source $(dirname $0)/common.sh
 repository=$(pwd)/deployment-repository
 
 pushd git-repo > /dev/null
+ulimit -n 65536
 ./gradlew --no-daemon -PdeploymentRepository=${repository} build publish
 popd > /dev/null
