dcd081e527542b133490dcd9e473e8867c4f48a5
Andy Wilkinson <wilkinsona@vmware.com>
2023-07-12 12:12:19 +0100

    Polish "Remove side-effect on bom ordering when configuring pom's dep mgmt"
    See gh-355

--- src/main/java/io/spring/gradle/dependencymanagement/internal/StandardPomDependencyManagementConfigurer.java
+++ src/main/java/io/spring/gradle/dependencymanagement/internal/StandardPomDependencyManagementConfigurer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2022 the original author or authors.
+ * Copyright 2014-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -144,13 +144,10 @@ public class StandardPomDependencyManagementConfigurer implements PomDependencyM
 		for (Dependency override : overrides) {
 			appendDependencyNode(dependencies, override.getCoordinates(), override.getScope(), override.getType());
 		}
-		List<PomReference> importedBoms = this.dependencyManagement.getImportedBomReferences();
-
-		List<PomReference> importedBomsCopy = new ArrayList<>(importedBoms);
-		Collections.reverse(importedBomsCopy);
-
-		for (PomReference resolvedBom : importedBomsCopy) {
-			addImport(dependencies, resolvedBom);
+		List<PomReference> importOrderBomReferences = new ArrayList<>(bomReferences);
+		Collections.reverse(importOrderBomReferences);
+		for (PomReference bomReference : importOrderBomReferences) {
+			addImport(dependencies, bomReference);
 		}
 	}
 
